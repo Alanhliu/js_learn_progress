a501e7176ca34b9f4033c61dbfddbc7b8b14dbf9
Hao Liu <haoliu@chuanliu.com.cn>
2016-11-03 19:44:29 +0800

    demo_ajax

---
+++ demo_ajax/ajax.js
@@ -0,0 +1,52 @@
+/**
+ * Created by hliu on 2016/10/30.
+ */
+
+
+document.addEventListener('click',function() {
+
+    /*
+     //同步
+     var xhr = new XMLHttpRequest();
+
+     xhr.open('get','ajax_response.html',false);
+
+     xhr.send(null);
+
+     alert(xhr.responseText);
+     alert(xhr.status);
+
+     //异步
+     var xhr = new XMLHttpRequest();
+
+     xhr.onreadystatechange = function() {
+     if (xhr.readyState == 4) {
+     alert(xhr.responseText);
+     alert(xhr.status);
+     }
+     };
+
+     xhr.open('get','ajax_response.html',true);
+     xhr.send(null);
+     xhr.abort();//取消异步请求
+
+     //响应头信息,请求头信息
+     var xhr = new XMLHttpRequest();
+
+     xhr.onreadystatechange = function() {
+     if (xhr.readyState == 4) {
+     //响应头信息
+     alert(xhr.getAllResponseHeaders());
+     alert(xhr.getResponseHeader('Content-type'));
+     }
+     };
+
+     xhr.open('get','ajax_response.html',true);
+     //请求头信息
+     xhr.setRequestHeader('myheader','lee');
+     xhr.send(null);
+    */
+
+    //get post
+
+});
